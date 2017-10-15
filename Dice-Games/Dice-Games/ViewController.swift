ff93e85d828c89e99780a0f18b534c3872fc8e22
alumincan <alumincan@gmail.com>
2017-10-16 02:22:35 +0800

    Dice Game

---
+++ Dice-Games/Dice-Games/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Dice-Games
+//
+//  Created by alumincan on 2017/10/15.
+//  Copyright © 2017年 sample. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+
+    var randomDiceIndex1 : Int = 0
+    var randomDiceIndex2 : Int = 0
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+
+    @IBAction func rollButton(_ sender: UIButton) {
+        randomDiceIndex1 = Int (arc4random_uniform(6));
+        randomDiceIndex2 = Int (arc4random_uniform(6));
+        diceImageView1.image = UIImage(named: "Dice\(randomDiceIndex1 + 1)");
+        diceImageView2.image = UIImage(named: "Dice\(randomDiceIndex2 + 1)");
+        print("Dice 1 = \(randomDiceIndex1 + 1), Dice 2 = \(randomDiceIndex2 + 1)");
+
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
