b15a87e1bd8a6dad33e3185429cd6817a588229d
alumincan <alumincan@gmail.com>
2017-10-17 01:02:47 +0800

    Dice game
    	Add Function for change dice view

--- Dice-Games/Dice-Games/ViewController.swift
+++ Dice-Games/Dice-Games/ViewController.swift
@@ -18,6 +18,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView2: UIImageView!
     let diceArray = ["Dice1", "Dice2", "Dice3", "Dice4", "Dice5", "Dice6"]
     @IBAction func rollButton(_ sender: UIButton) {
+        changeDiceView()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        changeDiceView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func changeDiceView()  {
         randomDiceIndex1 = Int (arc4random_uniform(UInt32(diceArray.count)));
         randomDiceIndex2 = Int (arc4random_uniform(UInt32(diceArray.count)));
         //diceImageView1.image = UIImage(named: "Dice\(randomDiceIndex1 + 1)");
@@ -25,17 +40,6 @@ class ViewController: UIViewController {
         diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
         diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
         //print("Dice 1 = \(randomDiceIndex1 + 1), Dice 2 = \(randomDiceIndex2 + 1)");
-
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 }
 
