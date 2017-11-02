2513eb4425a5face61c693c9bfac0fa718ff1768
alumincan <alumincan@gmail.com>
2017-11-03 00:46:42 +0800

    pratice Auto layout 2

---
+++ autoLayout/autoLayout/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  autoLayout
+//
+//  Created by alumincan on 2017/10/30.
+//  Copyright © 2017年 sample. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+//        let squre = UIView(frame: CGRect(x:view.frame.width / 2 - 25 , y: view.frame.height / 2 - 25, width: 50, height: 50))
+//        squre.backgroundColor = UIColor.red
+//        view.addSubview(squre)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
